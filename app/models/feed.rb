56a2f96f0ef83cedf150ad75a319af0307aa8e8e
Dylan Karr <dylan@dylankarr.com>
2016-08-06 11:00:58 -0400

    Added #fetch method to feed object
    I added the #fetch method to the feed object for fetching entries from
    an RSS feed and creating links from them.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -6,10 +6,17 @@ class Feed < ApplicationRecord
 
   before_create :set_title
 
+  def fetch
+    Feedjira::Feed.fetch_and_parse(url).entries.all? do |entry|
+      link = self.links.find_or_initialize_by(url: entry.url)
+      link.title = entry.title
+      link.save
+    end
+  end
+
   private
 
   def set_title
-    feed = Feedjira::Feed.fetch_and_parse url
-    self.title = feed.title
+    self.title = Feedjira::Feed.fetch_and_parse(url).title if title.nil?
   end
 end
--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Feed, type: :model do
+  describe '#fetch' do
+    let(:entry) { double() }
+
+    before do
+      allow(entry).to receive(:title) { Faker::Lorem.word }
+      allow(entry).to receive(:url)   { Faker::Internet.url }
+      allow(Feedjira::Feed).to receive(:fetch_and_parse) { |url| [entry] }
+    end
+
+    it 'creates a link' do
+      feed = create :feed
+      expect{ feed.fetch }.to change{ Link.count }.by(1)
+    end
+  end
 end
