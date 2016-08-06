6f947b298d39db8813480fde8657c6eff7685f07
Dylan Karr <dylan@dylankarr.com>
2016-08-06 09:50:33 -0400

    Added a callback to feeds to set title on create

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,4 +1,13 @@
 class Feed < ApplicationRecord
   validates :title, :url, presence: true
   validates :title, :url, uniqueness: true
+
+  before_create :set_title
+
+  private
+
+  def set_title
+    feed = Feedjira::Feed.fetch_and_parse url
+    self.title = feed.title
+  end
 end
