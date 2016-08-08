9b5c52359e075bf006c4d873fb7ddceb292ef417
Dylan Karr <dylan@dylankarr.com>
2016-08-08 15:39:05 -0400

    Added source links to links index page
    I added a links to the links on the index page to the feed path for
    sources. This way, users can click the name of a source and limit result
    to just that publisher.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,10 @@
 class Link < ApplicationRecord
+  delegate :title, to: :feed, prefix: true
+
   belongs_to :feed
 
   validates :title, :url, :feed_id, presence: true
   validates :title, :url, uniqueness: true
+
+  default_scope { includes(:feed) }
 end
