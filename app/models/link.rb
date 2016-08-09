b66a1e3eab1cd6c8e7abef1396593b7e23e952c6
Dylan Karr <dylan@dylankarr.com>
2016-08-08 23:48:43 -0400

    Added tagging to links based on proper nouns

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ApplicationRecord
+  acts_as_taggable
+
   delegate :title, to: :feed, prefix: true
 
   belongs_to :feed
@@ -6,5 +8,16 @@ class Link < ApplicationRecord
   validates :title, :url, :feed_id, presence: true
   validates :title, :url, uniqueness: true
 
+  after_create :extract_tags
+
   default_scope { includes(:feed) }
+
+  private
+
+  def extract_tags
+    tagger = Brill::Tagger.new
+    tags = tagger.tag(body).each do |term|
+      tag_list.add(term[0]) if term[1] == 'NNP'
+    end
+  end
 end
