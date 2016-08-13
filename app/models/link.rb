1e673b9ee5cab2757a497697c564d91a7b4f98d0
Dylan Karr <dylan@dylankarr.com>
2016-08-12 23:59:06 -0400

    Improved link body sanitization for tagging
    I added more regexes in a separate callback for cleaning up link bodies
    before processing them with the tagger. This should help improve the
    results we get for tags.

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,15 +8,30 @@ class Link < ApplicationRecord
   validates :title, :url, :feed_id, presence: true
   validates :title, :url, uniqueness: true
 
+  before_validation :sanitized_body
   after_create :extract_tags
 
   default_scope { includes(:feed) }
 
   private
 
+  def sanitized_body
+    # Strip html entities
+    self.body = body.gsub(/&[0-9#]+;/, '')
+
+    # Strip html tags
+    self.body = body.gsub(/\<[^\>]+\>/, '')
+
+    # Strip non-ascii characters
+    self.body = body.gsub(/[^[:ascii:]]/, '')
+
+    # Collapse whitespace
+    self.body = body.gsub(/\s+/, ' ')
+  end
+
   def extract_tags
-    sanitized_body = body.gsub(/[^\s\w]+/, '')
-    tags = Brill::Tagger.new.tag(sanitized_body)
-    tag_list = tags.select{ |t| t[1] == 'NNP' }.map{ |t| t[0] }
+    # Tag all proper nouns in the body
+    tags = Brill::Tagger.new.tag(body)
+    self.tag_list = tags.select{ |t| t[1] == 'NNP' }.map{ |t| t[0] }
   end
 end
