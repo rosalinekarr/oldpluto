2b15bbbe5cb84d6bee28e2009f71ec31476d805c
Dylan Karr <dylan@dylankarr.com>
2016-08-24 22:23:21 -0400

    Changed feed params to source
    I renamed the feed param on links index to source and changed it to
    search by id rather than slug name so it won't need to make another
    database call.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,14 +2,8 @@ class LinksController < ApplicationController
   def index
     @links = Link.includes(:feed)
 
-    if params[:tag].present?
-      @links = @links.tagged_with(params[:tag])
-    end
-
-    if params[:feed].present?
-      @feed = Feed.friendly.find(params[:feed])
-      @links = @links.where(feed: @feed)
-    end
+    @links = @links.tagged_with(params[:tag])       if params[:tag].present?
+    @links = @links.where(feed_id: params[:source]) if params[:source].present?
 
     @params = params.permit(:feed, :tag)
     @links = @links.order(published_at: :desc).page params[:page]
