b8787fcf6c644bee944a76fd01be8f1391ff80f6
Dylan Karr <dylan@dylankarr.com>
2016-08-27 22:23:28 -0400

    Added authorization to links controller
    I restricted the links controller to require a user account when
    proceeding past the first page.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,13 +5,21 @@ class LinksController < ApplicationController
     @links = @links.tagged_with(params[:tag])       if params[:tag].present?
     @links = @links.where(feed_id: params[:source]) if params[:source].present?
 
+    authenticate_user! if page != 1
+
     @params = params.permit(:feed, :tag)
-    @links = @links.order(published_at: :desc).page params[:page]
+    @links = @links.order(published_at: :desc).page page
   end
 
   def show
     @link = Link.find(params[:id])
     @link.update(visits: @link.visits + 1)
     redirect_to @link.url
+  end
+
+  private
+
+  def page
+    params[:page] || 1
   end
 end
