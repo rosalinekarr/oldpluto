12c2311308f26008717136ed5b8c716c5f86cb66
Dylan Karr <dylan@dylankarr.com>
2016-08-31 19:54:32 -0400

    Added a max and min delay to fetch link jobs
    I added a maximum value and minimum value for the delay between RSS feed
    fetches. The max will be 3 days and the minimum will be 5 minutes.

--- app/jobs/fetch_links_job.rb
+++ app/jobs/fetch_links_job.rb
@@ -1,9 +1,13 @@
 class FetchLinksJob < ApplicationJob
+  MAX_DELAY = 4320 # Minutes in 3 days
+  MIN_DELAY = 5
+
   queue_as :default
 
-  def perform(feed_id, frequency)
+  def perform(feed_id, delay)
     count = Feed.find(feed_id).fetch
-    new_frequency = (count > 0) ? (frequency.to_f / count).ceil : (frequency * 2)
-    FetchLinksJob.set(wait: new_frequency.minutes).perform_later(feed_id, new_frequency)
+    new_delay = (count > 0) ? (delay.to_f / count).ceil : (delay * 2)
+    new_delay = [[new_delay, MAX_DELAY].min, MIN_DELAY].max
+    FetchLinksJob.set(wait: new_delay.minutes).perform_later(feed_id, new_delay)
   end
 end
