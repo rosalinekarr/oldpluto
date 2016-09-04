7aa833dca4c6a0cb12a4346399b6b0a19016ee7b
Dylan Karr <dylan@dylankarr.com>
2016-09-04 17:05:40 -0400

    Added specs for link model before validation hooks

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,4 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
+  it 'santizes html entities from the title' do
+    link = build :link, title: 'Cats &amp; Dogs'
+    link.valid?
+    expect(link.title).to eq('Cats & Dogs')
+  end
+
+  it 'santizes html from the title' do
+    link = build :link, title: '<p>Cats and Dogs</p>'
+    link.valid?
+    expect(link.title).to eq('Cats and Dogs')
+  end
+
+  it 'santizes html entities from the body' do
+    link = build :link, body: 'Cats &amp; Dogs'
+    link.valid?
+    expect(link.body).to eq('Cats & Dogs')
+  end
+
+  it 'santizes html from the body' do
+    link = build :link, body: '<p>Cats and Dogs</p>'
+    link.valid?
+    expect(link.body).to eq('Cats and Dogs')
+  end
 end
