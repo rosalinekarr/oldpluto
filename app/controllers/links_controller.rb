cb163154fb758e7c1226442a65458dc9fd40d99b
Dylan Karr <dylan@dylankarr.com>
2016-10-07 14:36:29 -0400

    Refactored link sharing

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -22,30 +22,11 @@ class LinksController < ApplicationController
   end
 
   def share
-    @link = Link.find(params[:link_id])
-    Share.create user: current_user, link: @link, network: params[:network]
-    if params[:network] == 'facebook'
-      redirect_to "https://www.facebook.com/sharer.php?u=#{ERB::Util.url_encode @link.url}"
-    elsif params[:network] == 'twitter'
-      redirect_to "https://twitter.com/intent/tweet?url=#{ERB::Util.url_encode @link.url}"
-    elsif params[:network] == 'google'
-      redirect_to "https://plus.google.com/share?url=#{ERB::Util.url_encode @link.url}"
-    elsif params[:network] == 'reddit'
-      redirect_to "https://www.reddit.com/submit?url=#{ERB::Util.url_encode @link.url}"
-    elsif params[:network] == 'tumblr'
-      redirect_to "https://www.tumblr.com/widgets/share/tool?canonicalUrl=#{ERB::Util.url_encode @link.url}"
-    elsif params[:network] == 'pinterest'
-      redirect_to "https://pinterest.com/pin/create/bookmarklet/?url=#{ERB::Util.url_encode @link.url}"
-    elsif params[:network] == 'linkedin'
-      redirect_to "https://www.linkedin.com/shareArticle?url=#{ERB::Util.url_encode @link.url}"
-    elsif params[:network] == 'buffer'
-      redirect_to "https://buffer.com/add?url=#{ERB::Util.url_encode @link.url}"
-    elsif params[:network] == 'digg'
-      redirect_to "http://digg.com/submit?url=#{ERB::Util.url_encode @link.url}"
-    elsif params[:network] == 'stumbleupon'
-      redirect_to "http://www.stumbleupon.com/submit?url=#{ERB::Util.url_encode @link.url}"
-    elsif params[:network] == 'delicious'
-      redirect_to "https://delicious.com/save?v=5&url=#{ERB::Util.url_encode @link.url}"
+    @share = Share.new user: current_user,
+                       link_id: params[:link_id],
+                       network: params[:network]
+    if @share.save
+      redirect_to @share.url
     else
       raise ActionController::RoutingError.new('Not Found')
     end
--- app/models/share.rb
+++ app/models/share.rb
@@ -1,4 +1,32 @@
 class Share < ApplicationRecord
   belongs_to :user
   belongs_to :link, counter_cache: true
+
+  validates :network, presence: true
+
+  def url
+    if network == 'facebook'
+      "https://www.facebook.com/sharer.php?u=#{ERB::Util.url_encode link.url}"
+    elsif network == 'twitter'
+      "https://twitter.com/intent/tweet?url=#{ERB::Util.url_encode link.url}"
+    elsif network == 'google'
+      "https://plus.google.com/share?url=#{ERB::Util.url_encode link.url}"
+    elsif network == 'reddit'
+      "https://www.reddit.com/submit?url=#{ERB::Util.url_encode link.url}"
+    elsif network == 'tumblr'
+      "https://www.tumblr.com/widgets/share/tool?canonicalUrl=#{ERB::Util.url_encode link.url}"
+    elsif network == 'pinterest'
+      "https://pinterest.com/pin/create/bookmarklet/?url=#{ERB::Util.url_encode link.url}"
+    elsif network == 'linkedin'
+      "https://www.linkedin.com/shareArticle?url=#{ERB::Util.url_encode link.url}"
+    elsif network == 'buffer'
+      "https://buffer.com/add?url=#{ERB::Util.url_encode link.url}"
+    elsif network == 'digg'
+      "http://digg.com/submit?url=#{ERB::Util.url_encode link.url}"
+    elsif network == 'stumbleupon'
+      "http://www.stumbleupon.com/submit?url=#{ERB::Util.url_encode link.url}"
+    elsif network == 'delicious'
+      "https://delicious.com/save?v=5&url=#{ERB::Util.url_encode link.url}"
+    end
+  end
 end
