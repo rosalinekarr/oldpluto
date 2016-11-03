e4bf736c731939365c78a4d23e2ecb17654ed064
Dylan Karr <dylan@dylankarr.com>
2016-11-02 20:09:25 -0400

    Removed tag blacklisting

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,8 +1,6 @@
 require 'htmlentities'
 
 class Link < ApplicationRecord
-  BLACKLISTED_TAGS = YAML.load_file(Rails.root.join('config', 'tags', 'blacklist.yml'))
-
   acts_as_taggable
 
   delegate :title, to: :feed, prefix: true
@@ -26,19 +24,15 @@ class Link < ApplicationRecord
   private
 
   def sanitized_attributes
-    # Remove html tags
     self.title = ActionController::Base.helpers.strip_tags(title)
-    self.body = ActionController::Base.helpers.strip_tags(body)
-    # Convert html entities to unicode
+    self.body  = ActionController::Base.helpers.strip_tags(body)
+
     self.title = HTMLEntities.new.decode(title)
-    self.body = HTMLEntities.new.decode(body)
+    self.body  = HTMLEntities.new.decode(body)
   end
 
   def extract_tags
-    # Fetch all capitalized words from the title
-    tags = [title, body].join(' ').scan(/[A-Za-z]+/).map(&:downcase).uniq
-    # Set tags filtering with the tag blacklist
-    self.tag_list = tags.reject{ |tag| BLACKLISTED_TAGS.include? tag }
+    self.tag_list = [title, body].join(' ').scan(/[A-Za-z]+/).map(&:downcase).uniq
   end
 
   def set_expiration
