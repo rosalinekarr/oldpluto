4acbd19a9f16f787a618db72a6c4100988141f7c
Dylan Karr <dylan@dylankarr.com>
2016-11-13 21:45:33 -0500

    Added spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { is_expected.to have_many(:clicks) }
+  it { is_expected.to have_many(:impressions) }
+  it { is_expected.to have_many(:shares) }
 end
