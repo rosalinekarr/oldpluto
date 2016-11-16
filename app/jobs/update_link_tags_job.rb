7864f1b352624dd199df9f567949389386770bfe
Dylan Karr <dylan@dylankarr.com>
2016-11-16 18:28:31 -0500

    Lowered default tag count from 10 to 5

--- app/jobs/update_link_tags_job.rb
+++ app/jobs/update_link_tags_job.rb
@@ -5,7 +5,7 @@ class UpdateLinkTagsJob < ApplicationJob
     link = Link.find(link_id)
     link.tag_list = link.corpus.uniq.sort_by{ |tag|
       $redis.get(tag).to_f / link.corpus.count(tag)
-    }.first(10)
+    }.first(5)
     link.save
   end
 end
