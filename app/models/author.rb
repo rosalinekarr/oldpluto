4834d4c128413b71b742380158c860d250c33435
Dylan Karr <dylan@dylankarr.com>
2016-11-18 23:26:19 -0500

    Added friendly id slugging to author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,11 @@
 class Author < ApplicationRecord
+  extend FriendlyId
+
   has_many :links, dependent: :destroy
 
   validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :slug, presence: true
+  validates :slug, uniqueness: true
+
+  friendly_id :name, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119022059) do
+ActiveRecord::Schema.define(version: 20161119041735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20161119022059) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "links_count", default: 0, null: false
+    t.string   "slug"
+    t.index ["slug"], name: "index_authors_on_slug", unique: true, using: :btree
   end
 
   create_table "clicks", force: :cascade do |t|
