2021996ba4571b17ce96baf6490158298a412893
Dylan Karr <dylan@dylankarr.com>
2016-12-10 21:44:36 -0500

    Added click count tracking to click model

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,4 +1,16 @@
 class Click < ApplicationRecord
   belongs_to :user
   belongs_to :link, counter_cache: true
+
+  after_create :increment_click_counts
+
+  private
+
+  def increment_click_counts
+    words = [link.title, link.body].join(' ').scan(/[A-Za-z]+/).map(&:downcase)
+    return if words.empty?
+    $redis.zadd("clicks:#{id}", words.uniq.map{ |word| [words.count(word), word] })
+    $redis.zunionstore('clicks', ['clicks', "clicks:#{id}"])
+    $redis.expire("clicks:#{id}", 0)
+  end
 end
