ea952916ca73df7591d953b4d8465c74aff7c872
Dylan Karr <dylan@dylankarr.com>
2016-12-12 19:55:32 -0500

    Moved word count incrementation into a background job

---
+++ app/jobs/increment_word_counts_job.rb
@@ -0,0 +1,11 @@
+class IncrementWordCountsJob < ApplicationJob
+  queue_as :tags
+
+  def perform(*words)
+    word_counts = words.uniq.map{ |word| [words.count(word), word] }
+    $redis.zadd("corpus:temp", word_counts)
+    $redis.zunionstore('corpus', ['corpus', "corpus:temp"])
+    $redis.expire("corpus:temp", 0)
+    UpdateTagScoresJob.perform_later
+  end
+end
