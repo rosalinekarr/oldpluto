bac345071f47e51b0554425caefd28de2c5f4386
Dylan Karr <dylan@dylankarr.com>
2016-12-18 16:34:28 -0500

    Refactored tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,18 +1,11 @@
 class Tag
   def self.scores
     @scores ||= begin
-      corpus = Hash[Tag.corpus]
-      scores = Tag.clicks.sort_by do |word, score|
+      clicks = $redis.zrange('clicks', 0, -1, withscores: true)
+      corpus = Hash[$redis.zrange('corpus', 0, -1, withscores: true)]
+      scores = clicks.sort_by do |word, score|
         score * 1.0 / (corpus[word] || 1.0)
       end
     end
-  end
-
-  def self.corpus
-    @corpus ||= $redis.zrange('corpus', 0, -1, withscores: true)
-  end
-
-  def self.clicks
-    @clicks ||= $redis.zrange('clicks', 0, -1, withscores: true)
   end
 end
