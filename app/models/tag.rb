1c7f94268a251bfa47229f96df2a587e0dbc22c2
Dylan Karr <dylan@dylankarr.com>
2016-12-17 19:24:20 -0500

    Fixed bugs related to tag methods

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,38 +1,34 @@
 class Tag
   def self.increment_tag_counts(words)
-    Tag.update_set_counts('corpus', Tag.tags_from_words(words)) if words.any?
+    word_counts = words.uniq.map{ |word| [ words.count(word), word ] }
+    Tag.update_set_counts('corpus', word_counts) if words.any?
   end
 
   def self.decrement_tag_counts(words)
-    Tag.update_set_counts('corpus', Tag.tags_from_words(words, -1)) if words.any?
+    word_counts = words.uniq.map{ |word| [ -1 * words.count(word), word ] }
+    Tag.update_set_counts('corpus', word_counts) if words.any?
   end
 
   def self.increment_click_counts(words)
-    Tag.update_set_counts('clicks', Tag.tags_from_words(words)) if words.any?
+    word_counts = words.uniq.map{ |word| [ words.count(word), word ] }
+    Tag.update_set_counts('clicks', word_counts) if words.any?
   end
 
   def self.decrement_click_counts(words)
-    Tag.update_set_counts('clicks', Tag.tags_from_words(words, -1)) if words.any?
+    word_counts = words.uniq.map{ |word| [ -1 * words.count(word), word ] }
+    Tag.update_set_counts('clicks', word_counts) if words.any?
   end
 
   def self.update_set_counts(set, counts)
     $redis.zadd("#{set}:temp", counts)
     $redis.zunionstore(set, [set, "#{set}:temp"])
     $redis.expire("#{set}:temp", 0)
-    Tag.update_tag_scores!
-  end
-
-  def self.update_tag_scores!
     click_counts = $redis.zrange('clicks', 0, -1, withscores: true)
     word_counts  = $redis.zrange('corpus', 0, -1, withscores: true)
-    corpus = Hash[word_counts.each_slice(2).to_a]
+    corpus = Hash[word_counts]
     scores = click_counts.map do |count|
       [count[1] * 1.0 / (corpus[count[0]] || 1.0), count[0]]
     end
     $redis.zadd('scores', scores) if scores.any?
-  end
-
-  def self.tags_from_words(words, weight=1)
-    words.uniq.map{ |word| [ weight * words.count(word), word ] }
   end
 end
