da78fba8ff3ec543a35fb8d7681ef9c00796255e
Dylan Karr <dylan@dylankarr.com>
2016-12-18 22:45:47 -0500

    Refactored tags to use zscore instead of zrange

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,9 +2,8 @@ class Tag
   def self.scores
     @scores ||= begin
       clicks = $redis.zrange('clicks', 0, -1, withscores: true)
-      corpus = Hash[$redis.zrange('corpus', 0, -1, withscores: true)]
       scores = clicks.map do |word, score|
-        [word, score * 1.0 / (corpus[word] || 1.0)]
+        [word, score * 1.0 / ($redis.zscore('corpus', word) || 1.0)]
       end
       Hash[scores]
     end
