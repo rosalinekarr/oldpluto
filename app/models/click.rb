7364683135e56888d6de353dbf059d672c410870
Dylan Karr <dylan@dylankarr.com>
2017-01-26 11:02:51 -0500

    Added tag click count incrementation to clicks

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,4 +1,13 @@
 class Click < ApplicationRecord
   belongs_to :user
   belongs_to :link, counter_cache: true
+
+  after_create :increment_click_counts
+
+  private
+
+  def increment_click_counts
+    corpus = (link.title.scan(/\w+/) + link.body.scan(/\w+/))
+    link.tags.each{ |tag| tag.increment!(:click, corpus.count(tag.name)) }
+  end
 end
