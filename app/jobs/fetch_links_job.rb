a109158c879ca80b2608ce078c1d2ad73c7d4271
Dylan Karr <dylan@dylankarr.com>
2017-01-26 11:57:21 -0500

    Added exception handling for link fetching

--- app/jobs/fetch_links_job.rb
+++ app/jobs/fetch_links_job.rb
@@ -1,13 +1,25 @@
 class FetchLinksJob < ApplicationJob
-  MAX_DELAY = 259200 # Minutes in 3 days
-  MIN_DELAY = 300
+  MAX_DELAY = 3.days
+  MIN_DELAY = 5.minutes
+
+  rescue_from(Feedjira::NoParserAvailable) do
+    retry_job wait: MAX_DELAY
+  end
+
+  rescue_from(Feedjira::FetchFailure) do
+    retry_job wait: MAX_DELAY
+  end
+
+  rescue_from(Faraday::ConnectionFailed) do
+    retry_job wait: MAX_DELAY
+  end
 
   queue_as :fetch
 
   def perform feed_id
     feed = Feed.find(feed_id)
     feed.fetch
-    delay = [[feed.publish_rate, MIN_DELAY.seconds].max, MAX_DELAY.seconds].min
+    delay = [[feed.publish_rate, MIN_DELAY].max, MAX_DELAY].min
     FetchLinksJob.set(wait: delay).perform_later feed_id
   end
 end
