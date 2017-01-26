cb49717ebe438662f6c13ee4b4826683decd6672
Dylan Karr <dylan@dylankarr.com>
2017-01-25 22:08:42 -0500

    Removed click tag incrementation

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,12 +1,4 @@
 class Click < ApplicationRecord
   belongs_to :user
   belongs_to :link, counter_cache: true
-
-  after_create :increment_click_counts
-
-  private
-
-  def increment_click_counts
-    link.corpus.each{ |tag, score| $redis.zincrby('clicks', score, tag) }
-  end
 end
