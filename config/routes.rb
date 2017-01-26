feec0750625377fd91f90d9f44d3f84b3353f584
Dylan Karr <dylan@dylankarr.com>
2017-01-25 22:02:17 -0500

    Added sidekiq monitor for development

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users
 
@@ -6,6 +8,8 @@ Rails.application.routes.draw do
   resources :links,          only:   [:index, :show] do
     get  '/share/:network', to: 'links#share',      as: 'share'
   end
+
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
 
   get '/legal', to: 'pages#legal'
   root to: 'pages#home'
