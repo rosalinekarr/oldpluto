7f6ec8454440e585b33fde531e3d929237001a15
Dylan Karr <dylan@dylankarr.com>
2017-01-30 22:01:15 -0500

    Removed tests for no longer present features

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,29 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  describe 'sanitizes attributes' do
-    it 'sanitizes html entities from the title' do
-      link = build :link, title: 'Cats &amp; Dogs'
-      link.valid?
-      expect(link.title).to eq('Cats & Dogs')
-    end
-
-    it 'sanitizes html from the title' do
-      link = build :link, title: '<p>Cats and Dogs</p>'
-      link.valid?
-      expect(link.title).to eq('Cats and Dogs')
-    end
-
-    it 'sanitizes html entities from the body' do
-      link = build :link, body: 'Cats &amp; Dogs'
-      link.valid?
-      expect(link.body).to eq('Cats & Dogs')
-    end
-
-    it 'sanitizes html from the body' do
-      link = build :link, body: '<p>Cats and Dogs</p>'
-      link.valid?
-      expect(link.body).to eq('Cats and Dogs')
-    end
-  end
 end
