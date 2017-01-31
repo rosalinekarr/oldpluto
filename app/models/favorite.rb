ba8cce9a00b8a6b47062c55857cff97fc09e9801
Dylan Karr <dylan@dylankarr.com>
2017-01-30 22:12:32 -0500

    Added search update job to favorite model

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,12 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :link, counter_cache: true
+
+  after_save :update_search_index
+
+  private
+
+  def update_search_index
+    Link.start_index_job(link)
+  end
 end
