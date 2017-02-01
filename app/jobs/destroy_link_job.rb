90b767769fc176c8024f2c3f1eef3aeca1ed3454
Dylan Karr <dylan@dylankarr.com>
2017-01-31 21:45:33 -0500

    Added index deletion to destroy links job

--- app/jobs/destroy_link_job.rb
+++ app/jobs/destroy_link_job.rb
@@ -2,6 +2,7 @@ class DestroyLinkJob < ApplicationJob
   queue_as :cleanup
 
   def perform(link_id)
+    Algolia::Index.new("Link_#{Rails.env}").delete_object(link_id)
     Link.find(link_id).destroy
   end
 end
