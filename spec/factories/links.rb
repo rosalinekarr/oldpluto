ce9716c06735bf6b23f1ab2607308c4a91303952
Dylan Karr <dylan@dylankarr.com>
2017-02-02 11:16:00 -0500

    Fixed bug in links factory

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :link do
     feed
-    title { Faker::Lorem.sentence }
-    url   { Faker::Internet.url }
+    title        { Faker::Lorem.sentence }
+    url          { Faker::Internet.url }
+    published_at { Faker::Time.backward(7) }
   end
 end
