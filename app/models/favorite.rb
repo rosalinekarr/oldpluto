7542f6b8abe0246906cc2fb17e04cf45e5a533e6
Dylan Karr <dylan@dylankarr.com>
2017-02-02 22:08:28 -0500

    Added check to favorites to destroy expired links

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,12 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :link, counter_cache: true
+
+  before_destroy :delete_link_if_expired
+
+  private
+
+  def delete_link_if_expired
+    link.destroy if link.favorites_count < 2 && link.published_at < Link::TTL.ago
+  end
 end
