c305c3103bf266311af644fdbd26220e6eb0e86c
Dylan Karr <dylan@dylankarr.com>
2017-02-03 13:34:24 -0500

    Updated impression model to reindex links

--- app/models/impression.rb
+++ app/models/impression.rb
@@ -1,4 +1,12 @@
 class Impression < ApplicationRecord
   belongs_to :user
   belongs_to :link, counter_cache: true
+
+  after_create :reindex_link
+
+  private
+
+  def reindex_link
+    link.index!
+  end
 end
