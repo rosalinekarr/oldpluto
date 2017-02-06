88e3a5fd7b569eee7013e1d97c8b02b2d299e316
Dylan Karr <dylan@dylankarr.com>
2017-02-06 08:08:59 -0500

    Moved link reindexing into a transaction

--- app/jobs/create_impression_job.rb
+++ app/jobs/create_impression_job.rb
@@ -2,8 +2,10 @@ class CreateImpressionJob < ApplicationJob
   queue_as :search
 
   def perform(user_id, link_ids)
-    Link.where(id: link_ids).each do |link|
-      Impression.create link: link, user_id: user_id
+    ActiveRecord::Base.transaction do
+      Link.where(id: link_ids).each do |link|
+        Impression.create link: link, user_id: user_id
+      end
     end
   end
 end
